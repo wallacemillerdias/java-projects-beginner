5b75e58864f22dc8817cab5086d8a0f98d618af4
Wallace Miller Dias <wallacemillerdias@MacBook-Air-de-Wallace.local>
2025-02-27 15:30:49 -0300

    Class Task

---
+++ todo-list/src/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    @Override
+    public String toString() {
+        return description + (completed ? "[completed]": "");
+    }
+}
